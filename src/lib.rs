c312a9ed20e4652f1c1bfb236ed26973dabd39ba
viniciusfonseca <vfonseca1618@gmail.com>
2025-04-12 21:20:55 -0300

    added unit testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,26 @@
 use waki::{handler, ErrorCode, Request, Response};
 
 #[handler]
-fn hello(_req: Request) -> Result<Response, ErrorCode> {
+fn h(r: Request) -> Result<Response, ErrorCode> { http_handler(r) }
+
+pub fn http_handler(_req: Request) -> Result<Response, ErrorCode> {
     println!("Data from stdout");
     Response::builder()
-        .body(format!(
-            "Hello World!"
-        ))
+        .body(format!("Hello World!"))
         .build()
+}
+
+#[cfg(test)]
+mod tests {
+    use waki::Request;
+    use waki::Method::*;
+    use crate::http_handler;
+
+    #[test]
+    fn test_hello() {
+        let req = Request::builder(Get, "http://localhost:8080").build().unwrap();
+        let res = http_handler(req).unwrap();
+        assert_eq!(res.status_code(), 200);
+        assert_eq!(res.body().unwrap(), b"Hello World!");
+    }
 }
--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-raikiri component run --request "`cat request.json`"
+raikiri run
+
+curl http://localhost:3000 \
+  --header "Content-Type: application/json" \
+  --header "Platform-Command: invoke" \
+  --header "Invoke-Component: hello" \
+  --header "Invoke-Method: GET"
